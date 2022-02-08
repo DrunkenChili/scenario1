eaee51fe58cefd5eaaf4dbbc414b5b6c43785d2f
Derek Hwang <opensource@zijian.me>
2022-02-08 23:12:50 +0800

    Completed the main() function and server module

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+mod server;
+mod router;
+mod server:Server;
+
+fn main(){
+    let server = Server::new("localhost:3000");
+    server.run();
 }
---
+++ httpserver/src/server.rs
@@ -0,0 +1,28 @@
+use super::router::Router;
+use http::httprequest::HttpRequest;
+use std::net::TcpListener;
+
+pub struct Server<'a> {
+    socket_addr: &'a str,
+}
+
+impl<'a> Server<'a> {
+    pub fn new(socket_addr: &'a str) -> Self {
+        Server {
+            socket_addr
+        }
+    }
+
+    pub fn run(&self) {
+        let connect_listener = TcpListener::bind(self.socket_addr).unwrap();
+        println!("Running on {}", self.socket_addr);
+        for stream in connect_listener.incoming() {
+            let mut stream = stream.unwrap();
+            println!("Connection established");
+            let mut read_buffer = [0; 90];
+            stream.read(&read_buffer).unwrap();
+            let req: HttpRequest = String::from_utf8(read_buffer.to_vec()).unwrap().into();
+            Router::route(req, &mut stream);
+        }
+    }
+}
