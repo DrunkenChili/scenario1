6a2b56db00f5a9cb196a283e688546db5b3de8a5
Derek Hwang <opensource@zijian.me>
2022-02-08 16:18:11 +0800

    cargo fmt

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -4,7 +4,7 @@ use std::net::TcpListener;
 fn main() {
     let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000");
-    for stream in connection_listener.incoming(){
+    for stream in connection_listener.incoming() {
         let mut stream = stream.unwrap();
         println!("Connection established");
         let mut buffer = [0; 1024];
