a1b146036c9e2a94346a4b5100ba51d8097ed0ac
Derek Hwang <opensource@zijian.me>
2022-02-08 17:06:47 +0800

    Add Version enum

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -5,12 +5,28 @@ pub enum Method {
     Uninitialized,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
 impl From<&str> for Method {
     fn from(s: &str) -> Self {
         match s {
             "GET" => Method::Get,
             "POST" => Method::Post,
             _ => Method::Uninitialized,
+        }
+    }
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Self {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
         }
     }
 }
@@ -23,5 +39,11 @@ mod tests {
     fn test_method_into() {
         let m: Method = "GET".into();
         assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let v: Version = "HTTP/1.1".into();
+        assert_eq!(v, Version::V1_1);
     }
 }
